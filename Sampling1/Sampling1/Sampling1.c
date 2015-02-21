3658cd32869311be510075b97f2a568a196c9bdf
Strice91 <r.strice@gmail.com>
2015-02-21 18:08:49 +0100

    Sampling Projekt angelegt

---
+++ Sampling1/Sampling1/Sampling1.c
@@ -0,0 +1,17 @@
+/*
+ * Sampling1.c
+ *
+ * Created: 21.02.2015 17:10:17
+ *  Author: Strice
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
