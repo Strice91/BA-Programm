61163455e21e7d0ba524939465ba7605b67cf621
Strice91 <r.strice@gmail.com>
2015-02-22 18:27:25 +0100

    sampling.h hinzugefügt

---
+++ Sampling1/Sampling1/Sampling1.h
@@ -0,0 +1,14 @@
+/*
+ * Sampling1.h
+ *
+ * Created: 22.02.2015 17:19:15
+ *  Author: Strice
+ */ 
+
+#include <avr/io.h>
+#include "../../inc/adc.h"
+#include "../../inc/lcd.h"
+#include "../../inc/rtc.h"
+#include "../../inc/debug.h"
+
+void sampling_init(void);
