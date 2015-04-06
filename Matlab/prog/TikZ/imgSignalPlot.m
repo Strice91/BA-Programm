7452379969c8bb075ab66b4ca15ce59377ea123e
Strice91 <r.strice@gmail.com>
2015-04-06 19:18:37 +0200

    signalplot updated

--- Matlab/prog/TikZ/imgSignalPlot.m
+++ Matlab/prog/TikZ/imgSignalPlot.m
@@ -20,32 +20,41 @@ U3 = U3(1:end-1);
 I3 = I3(1:end-1);
 x3 = (1:length(U3)) * 1e-4;
 
-figure;
-subplot(1,3,1);
+figure('Position',[0, 0, 350, 500]);
 [hAx,hLine1,hLine2] = plotyy(x1,U1,x1,I1);
-ylabel(hAx(1),'u(t) [V]') % left y-axis
-ylabel(hAx(2),'i(t) [A]') % right y-axis
+text(0,42,'u(t) [V]','color','b')
+text(0.033,42,'i(t) [A]','color',[0 0.502 0])
+%ylabel(hAx(1),'u(t) [V]') % left y-axis
+%ylabel(hAx(2),'i(t) [A]') % right y-axis
 % legend('u(t) [V]','i(t)  [A]')
 xlabel('t [s]')
-title('Eingangssignal');
+%title('Eingangssignal');
+matlab2tikz('../../../../tex/Dok/img/signalAufbereitung1.tikz');  
 
-subplot(1,3,2);
+figure('Position',[0, 0, 350, 500]);
 [hAx,hLine1,hLine2] = plotyy(x2,U2,x2,I2);
-ylabel(hAx(1),'u(t) [V]') % left y-axis
-ylabel(hAx(2),'u_s(t) [V]') % right y-axis
+text(0,2.1,'u(t) [V]','color','b')
+text(0.033,2.1,'u_s(t) [V]','color',[0 0.502 0])
+% ylabel(hAx(1),'u(t) [V]') % left y-axis
+% ylabel(hAx(2),'u_s(t) [V]') % right y-axis
 % legend('u(t)  [V]','u_s(t) [V]')
 xlabel('t [s]')
-title('Aufbereitung 1');
+%title('Aufbereitung 1');
+matlab2tikz('../../../../tex/Dok/img/signalAufbereitung2.tikz'); 
 
-subplot(1,3,3);
+figure('Position',[0, 0, 350, 500]);
 [hAx,hLine1,hLine2] = plotyy(x3,U3,x3,I3);
+text(0,3.18,'x_u(t) [V]','color','b')
+text(0.033,3.18,'x_i(t) [V]','color',[0 0.502 0])
 % ylabel(hAx(1),'x_u(t) [V]') % left y-axis
 % ylabel(hAx(2),'x_i(t) [V]') % right y-axis
 % legend('x_u(t) [V]','x_i(t)  [V]')
 xlabel('t [s]')
-title('Aufbereitung 2');
+%title('Aufbereitung 2');
 ylim(hAx(1),[0,3.1])
 ylim(hAx(2),[0,3.1])
+matlab2tikz('../../../../tex/Dok/img/signalAufbereitung3.tikz'); 
+
 % load('data\W01.mat');
 % U2 = U{12};
 % I2 = I{12};
