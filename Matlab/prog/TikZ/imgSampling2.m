ab3618255807bfc53f8c81ab3a29150543faab48
Strice91 <r.strice@gmail.com>
2015-04-09 08:38:43 +0200

    sampling plots

---
+++ Matlab/prog/TikZ/imgSampling2.m
@@ -0,0 +1,19 @@
+function imgSampling2()
+
+close all;
+load('data/M19.mat')
+u1 = U{8};
+i1 = I{8};
+u1 = u1(22:47);
+i1 = i1(22:47);
+x = (0:length(u1)-1)/500;
+figure;
+plot(x,u1,x,i1);
+ylim([0,1024]);
+xlabel('t [s]');
+ylabel('ADC');
+legend('x_u(t)','x_i(t)');
+title('24V - 1.34A');
+matlab2tikz('../../../../tex/Dok/img/sampling2.tikz'); 
+end
+
